61589f1968e0ff8b04b8aa4e1ed4b076801d0418
OmegaTroy <juli.mjoi44@gmail.com>
2024-07-02 22:42:27 -0300

    asdasd

---
+++ src/db.js
@@ -0,0 +1,26 @@
+import mongodb from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+
+const url = process.env.MONGODB_URI
+
+if (!url) {
+  console.error('Error: La variable de entorno MONGODB_URI no está definida');
+  process.exit(1);
+}
+
+const connectDb = async ()=>{
+  try{
+    await mongodb.connect(url)
+  }catch(err){
+    console.log(err)
+  }
+}
+
+mongodb.connection.on('error',err=>{
+  console.log(err)
+})
+
+export {connectDb,mongodb}
+
+
